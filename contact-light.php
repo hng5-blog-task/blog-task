c629577e9cb9760f5d26c3754998de2a3f94f9ce
Joshua <olajidejoshua4real@gmail.com>
2019-04-07 14:25:57 -0700

    Contact us light theme page

---
+++ contact-light.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css"> 
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/contact-light.css">
+    <title>HNG | Contact Us</title>
+    
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="left-content text-center">
+            <img class="profile-img" src="img/profile.png">
+          <h1 class="display-4">Paul Jephtah</h1>
+          <p>UX DESIGNER | PRODUCT DESIGNER | FRONTEND NEWBIE</p>
+          <p>I'm kinda weird in a very good way..... I design for survival</p>
+          <div class="mb-2">
+           <a class="profile-social-icon" href="#"><img  src="img/github-logo.svg"></a>
+                    <a  class="profile-social-icon" href="#"><img  src="img/dribble.svg"></a>
+                    <a  class="profile-social-icon" href="#"><img src="img/rss-symbol.svg"></a>
+          </div>
+          <div class="link">
+            <a href="" class="menu-item">Draft</a>
+            <a href="">My Blogs</a>
+            <a href="">Insights</a>
+            <a href="">Settings</a>
+          </div>
+          <hr>
+          <div class="others">
+            <a href="">English (US)</a>
+            <a href="">Privacy</a>
+            <a href="">Terms Of use</a>
+            <a href="">Contact</a>
+            <a href="">Advertise with us</a>
+          </div>
+        </div>
+      </div>
+      
+        <div class="col-lg-8">
+          <div class="right-content text-center">
+          <center>
+            <div class="form">
+            <form action="">
+              <h1>Get in Touch</h1>
+              <p>How can we help you? We'd love to hear from you</p>
+              <div class="input-group">
+                <label for="username"></label>
+                <input type="text" name="user" id="user" placeholder="Username" >
+              </div>
+               <div class="input-group">
+                <label for="subj"></label>
+                <input type="text" name="subj" id="subj" placeholder="Subject" >
+              </div>
+               <div class="input-group">
+                <label for="email"></label>
+                <input type="email" name="email" id="email" placeholder="Email Address" >
+              </div>
+               <div class="input-group">
+                <label for="msg"></label>
+                <textarea id="msg" name="msg"  placeholder="Message"></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">SEND</button>
+            </form>
+            </div>
+          </center>
+          </div>
+        
+      </div>
+    </div>
+     <footer class="text-center">
+      &copy; copyright 2019 | BLOG
+  </footer>
+  </div>
+ 
+
+<script src="js/jquery-331.min.js"></script>
+<script src="bootstrap-4.1.3-dist/js/bootstrap.min.css"></script>
+<script src="js/popper.min.js"></script>
+</body>
+</html>
